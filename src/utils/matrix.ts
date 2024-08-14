9679b86f6262bec18384887a6b732f8f63844f99
Fabs <gianfranco.aguirre.cruz@gmail.com>
2024-08-14 15:51:13 -0500

    👔 update matrix validation

--- src/utils/matrix.ts
+++ src/utils/matrix.ts
@@ -1,7 +1,9 @@
 export const isDiagonal = (matrix: Matrix) =>
   matrix.every((row, i) => row.every((val, j) => (i === j ? true : val === 0)));
 
-export const isSquare = (matrix: Matrix) => {
-  const size = matrix.length;
-  return matrix.every(row => row.length === size);
+export const isMatrix = (matrix: Matrix) => {
+  if (matrix.length === 0) return false;
+
+  const rowLength = matrix[0].length;
+  return rowLength > 0 && matrix.every(row => row.length === rowLength);
 };
--- src/validators/matrix.ts
+++ src/validators/matrix.ts
@@ -1,13 +1,10 @@
 import { array, custom, number, parse } from 'valibot';
 
-import { isSquare } from '@/utils/matrix';
+import { isMatrix } from '@/utils/matrix';
 
-const MatrixSchema = array(array(number()));
-const SquareMatrixSchema = custom<Matrix>(
-  data => isSquare(validateMatrix(data)),
-  'Invalid type. Expected a square matrix.',
+const MatrixSchema = custom<Matrix>(
+  data => isMatrix(parse(array(array(number())), data)),
+  'Invalid type. Expected a matrix.',
 );
 
-const validateMatrix = (data: unknown) => parse(MatrixSchema, data);
-export const validateSquareMatrix = (data: unknown) =>
-  parse(SquareMatrixSchema, data);
+export const validateMatrix = (data: unknown) => parse(MatrixSchema, data);
